29e93e8a80c223f2b79fe8d75c6108484673a1da
howpez <howpez@gmail.com>
2013-12-14 16:19:46 -0800

    adding basic enemy

---
+++ project/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+
+	public Vector2 speed = new Vector2(10, 10);
+	public Vector2 direction = new Vector2(-1, 0);
+	public bool isTurnBased = false;
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+
+	}
+}
